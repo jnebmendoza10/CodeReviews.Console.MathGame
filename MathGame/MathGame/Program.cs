dad9fb197708e49da4b2e0217c693ad194116eca
bernardmendoza <bernardmendoza@outlook.ph>
2024-08-31 01:18:25 +0800

    Added UI logic and call to GameManager

--- MathGame/MathGame/Program.cs
+++ MathGame/MathGame/Program.cs
@@ -1,47 +1,128 @@
-﻿namespace MathGame
+﻿using System.Diagnostics;
+using MathGame.Entities;
+using MathGame.Extensions;
+
+namespace MathGame
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            displayMenuHeader();
+
+            List<Game> history = new List<Game>();
+
+            while (true)
+            {
+                try
+                {
+                    displayUserChoices();
+                    var menuChoice = Console.ReadLine().ParseMenuChoice();
+
+                    if (menuChoice == Enums.Menu.Play)
+                    {
+                        displayDifficulty();
+                        var difficultyChoice = Console.ReadLine().ParseDifficultyChoice();
+
+                        displayOperations();
+                        var operationChoice = Console.ReadLine().ParseModeChoice();
+
+                        GameManager manager = new GameManager();
+
+                        Game game = new Game();
+                        game.Difficulty = difficultyChoice;
+                        game.Mode = operationChoice;
+                        game.DatePlayed = DateTime.Now;
+
+                        history.Add(game);
+
+                        var questions = manager.GenerateMathQuestions(
+                            operationChoice,
+                            difficultyChoice);
+
+                        int correctAnswers = 0;
+                        int wrongAnswers = 0;
+
+                        Stopwatch stopwatch = new Stopwatch();
+
+                        stopwatch.Start();
+
+                        foreach (var question in questions)
+                        {
+                            Console.Write(question.Question);
+                            int.TryParse(Console.ReadLine(), out int userAnswer);
+
+                            if (question.Answer.Equals(userAnswer))
+                            {
+                                correctAnswers++;
+                            }
+                            else
+                            {
+                                wrongAnswers++;
+                            }
+                        }
+                        
+                        stopwatch.Stop();
+
+                        var timeElapsed = Math.Round(stopwatch.Elapsed.TotalSeconds, 2);
+
+                        Console.WriteLine($"You have {correctAnswers} correct answers and {wrongAnswers} wrong answers");
+                        Console.WriteLine($"You have finished the game in { timeElapsed } seconds");
+                    }
+                    else if (menuChoice == Enums.Menu.History)
+                    {
+                        displayGameHistory(history);
+                    }
+                    else if ( menuChoice == Enums.Menu.Quit)
+                    {
+                        displayMenuFooter();
+                        return;
+                    }
+
+                    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
         }
 
-        static void displayMenuHeader()
+        private static void displayMenuHeader()
         {
             Console.WriteLine("****************************");
             Console.WriteLine("Welcome to the Math Game");
             Console.WriteLine("****************************");
         }
 
-        static void displayUserChoices()
+        private static void displayUserChoices()
         {
-            Console.WriteLine("Enter a number to select your choice:");
             Console.WriteLine("1. Play Now");
             Console.WriteLine("2. View Game History");
             Console.WriteLine("3. Exit");
+            Console.Write("Enter a number to select your choice: ");
         }
 
 
-        static void displayDifficulty()
+        private static void displayDifficulty()
         {
-            Console.WriteLine("Enter a number to select difficulty:");
             Console.WriteLine("1. Easy");
             Console.WriteLine("2. Medium");
             Console.WriteLine("3. Hard");
+            Console.Write("Enter a number to select difficulty: ");
         }
 
-        static void displayOperations()
+        private static void displayOperations()
         {
-            Console.WriteLine("Enter a number to select an operation:");
             Console.WriteLine("1. Addition");
             Console.WriteLine("2. Subtraction");
             Console.WriteLine("3. Multiplication");
             Console.WriteLine("4. Division");
             Console.WriteLine("5. Random");
+            Console.Write("Enter a number to select an operation: ");
         }
 
-        static void displayGameHistory(List<string> userHistory)
+        private static void displayGameHistory(List<Game> userHistory)
         {
             if (userHistory == null || userHistory.Count == 0) 
             {
@@ -50,13 +131,13 @@
                 return;
             }
 
-            foreach (string item in userHistory)
+            foreach (var item in userHistory)
             {
-                Console.WriteLine(item);
+                Console.WriteLine($"{ item.DatePlayed }: You have played { item.Mode.ParseModeChoiceToString() } with difficulty of {item.Difficulty.ParseDifficultyToString()}");
             }
         }
 
-        static void displayMenuFooter()
+        private static void displayMenuFooter()
         {
             Console.WriteLine("****************************");
             Console.WriteLine("Thank you for playing!");
